35b05559c3e70f8a7135bb4a912d4211a470133d
ZZRRegion <854974242@qq.com>
2020-02-27 07:58:56 +0800

    1111

--- Python/qrcode.py
+++ Python/qrcode.py
@@ -1,9 +1,10 @@
-from MyQR import myqr
-import time
+from MyQR import myqr #二维码
+import time #时间模块
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #图片显示
 import matplotlib.image as mping
 
+#判断是否包含中文字符串
 def is_contain_chinese(check_str):
     for ch in check_str:
         if u'\u4e00' <= ch <= u'\u9fff':
@@ -15,7 +16,7 @@ while True:
     if is_contain_chinese(code):
         print("不支持中文，请重新输入！")
         continue
-    fileName = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    fileName = time.strftime("%Y%m%d%H%M%S", time.localtime()) #格式化当前时间，作为文件名
     fileName += ".png"
     myqr.run(
         words = code,
@@ -26,7 +27,8 @@ while True:
         save_name= fileName,
         )
     print("已将字符串:{}成功生成并保存二维码图片:{}".format(code, fileName))
-    png = mping.imread(fileName)
+    png = mping.imread(fileName) #读取图片文件
     plt.imshow(png)
     plt.axis("off")
-    plt.show()
+    plt.title(code) #更改标题
+    plt.show() #显示图片
